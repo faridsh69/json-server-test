dff7759247bf512530eeff011f1dc85461bfc350
Farid Shahidi <shahidi@innoscripta.com>
2024-08-29 14:25:45 +0200

    fixed router

--- src/router.js
+++ src/router.js
@@ -4,10 +4,8 @@ var projectsRoute = require("./db/projects.json");
 var progressRoute = require("./db/progress.json");
 var costsRoute = require("./db/costs.json");
 var eventsFilter = require("./db/eventsFilter.json");
-var project1487Route = require("./db/project1487.json");
 var partnershipCosts1487 = require("./db/partnershipCosts1487.json");
 var contractors1487 = require("./db/contractors1487.json");
-var contractorsAllAnalysis1487 = require("./db/contractorsAllAnalysis1487.json");
 var eventsEntityWatchers1487 = require("./db/eventsEntityWatchers1487.json");
 
 module.exports = function () {
@@ -17,15 +15,6 @@ module.exports = function () {
     costs: costsRoute,
     progress: progressRoute,
     projects: projectsRoute,
-    "projects/1487": project1487Route,
-    contractors: {
-      all: {
-        analysis: contractorsAllAnalysis1487,
-      },
-    },
-    events: {
-      filter: eventsFilter,
-    },
     contractors: contractors1487,
     "partnership-costs": partnershipCosts1487,
     "entity-watchers": eventsEntityWatchers1487,
--- src/server.js
+++ src/server.js
@@ -3,7 +3,18 @@ var server = jsonServer.create();
 var router = jsonServer.router(require("./router.js")());
 var middlewares = jsonServer.defaults();
 
+var project1487Route = require("./db/project1487.json");
+var contractorsAllAnalysis1487 = require("./db/contractorsAllAnalysis1487.json");
+var eventsFilter = require("./db/eventsFilter.json");
+
 server.use(middlewares);
+
+server.get("/projects/1487", (_, res) => res.json(project1487Route));
+server.get("/events/filter", (_, res) => res.json(eventsFilter));
+server.get("/contractors/all/analysis", (_, res) =>
+  res.json(contractorsAllAnalysis1487)
+);
+
 server.use(router);
 server.listen(5000, function () {
   console.log("JSON Server is running");
